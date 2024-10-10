14cad140ee4fbf3322afe9a299edc598284111f0
Tanner Jones <tjones25@basecampcodingacademy.org>
2024-10-10 12:53:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import monthInfo from './monthInfo.js';
 
-function App() {
+const App = () => {
+  const [textBoxes, setTextBoxes] = useState([{ id: 1, month:monthInfo[month - 1].name, date: startDate }]);
+
+  const fixCountAscending = (iArray) => {
+    console.log(month)
+    console.log(iArray)
+    console.log(monthInfo[(month - 1)].numDays)
+    if (iArray > monthInfo[(month - 1)].numDays) {
+      month = month + 1;
+      if (month === 13){
+        console.log('DECEMBER')
+        month = 1;
+      }
+      currentDate = 1;
+    }
+  }
+  const fixCountDescending = (iArray) => {
+    console.log(month)
+    console.log(iArray)
+    console.log(monthInfo[(month - 1)].numDays)
+    if (iArray < 1) {
+      month = month - 1;
+      if (month === 0){
+        console.log('JANUARY')
+        month = 12;
+      }
+      currentDate = monthInfo[(month - 1)].numDays;
+    }
+  }
+
+  const addTextBox = () => {
+    currentDate = Number(currentDate) + Number(1)
+    fixCountAscending(currentDate)
+    setTextBoxes([...textBoxes, { id: textBoxes.length + 1, month:monthInfo[Number(month) - Number(1)].name, date: currentDate }]);
+  };
+  
+  const removeTextBox = () => {
+    if(textBoxes.length != 1){
+      currentDate = Number(currentDate) - Number(1)
+      fixCountDescending(currentDate)
+      setTextBoxes(textBoxes.slice(0,-1));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='primary'>
+      <h1>Schedule Planner</h1><br></br>
+	  <input type='text' placeholder='Trip Name' className='tripTitle'/>
+	  <p><button className='buttonSub'onClick={removeTextBox}>-</button><button className='buttonAdd'onClick={addTextBox}>+</button></p>
+      <ul className='calendar'>
+        {textBoxes.map((textBox) => (
+          <ul>
+			<table className='cBox'><textarea className='dateBox'
+              type="text"
+              //onChange={(event) => handleChange(textBox.id, event)}
+            /><br></br>
+			<p>{textBox.month + ' ' + textBox.date}</p></table>
+          </ul>
+        ))}
+      </ul>
     </div>
   );
+};
+
+var month = Number(window.prompt('Please enter a month (1-12)'))
+while(month < 1 || month > 12){
+  var month = Number(window.prompt('Please enter a valid month (1-12)'))
 }
+
+var startDate = Number(window.prompt('Please enter a start date'))
+while(startDate < 1 || startDate > monthInfo[(month - 1)].numDays){
+	var startDate = Number(window.prompt('Please enter a valid start date'))
+}
+var currentDate = startDate
 
 export default App;
---
+++ src/monthInfo.js
@@ -0,0 +1,63 @@
+const monthInfo = [
+    {
+        "month": 1,
+        "name": "January",
+        "numDays": 31
+    },
+    {
+        "month": 2,
+        "name": "February",
+        "numDays": 28
+    },
+    {
+        "month": 3,
+        "name": "March",
+        "numDays": 31
+    },
+    {
+        "month": 4,
+        "name": "April",
+        "numDays": 30
+    },
+    {
+        "month": 5,
+        "name": "May",
+        "numDays": 31
+    },
+    {
+        "month": 6,
+        "name": "June",
+        "numDays": 30
+    },
+    {
+        "month": 7,
+        "name": "July",
+        "numDays": 31
+    },
+    {
+        "month": 8,
+        "name": "August",
+        "numDays": 31
+    },
+    {
+        "month": 9,
+        "name": "September",
+        "numDays": 30
+    },
+    {
+        "month": 10,
+        "name": "October",
+        "numDays": 31
+    },
+    {
+        "month": 11,
+        "name": "November",
+        "numDays": 30
+    },
+    {
+        "month": 12,
+        "name": "December",
+        "numDays": 31
+    }
+]
+export default monthInfo
